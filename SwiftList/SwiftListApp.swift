3a600808a77c913d8b00b137035e22ca9f9005c8
Samuel Cardoso <scksamuel.sc@gmail.com>
2021-12-30 20:09:24 -0300

    Initial Commit

---
+++ SwiftList/SwiftListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftListApp.swift
+//  SwiftList
+//
+//  Created by Samuel Cardoso on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
