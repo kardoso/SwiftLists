c0b87e1a554f3edd39913b52090e23d4fa304cc7
Samuel Cardoso <scksamuel.sc@gmail.com>
2021-12-30 20:22:05 -0300

    Create a static list app

--- SwiftList/ContentView.swift
+++ SwiftList/ContentView.swift
@@ -7,10 +7,26 @@
 
 import SwiftUI
 
+struct Movie: Identifiable {
+    var id = UUID()
+    var title: String
+}
+
 struct ContentView: View {
+    @State var movies = [
+        Movie(title: "Episode IV - A New Hope"),
+        Movie(title: "Episode V - The Empire Strikes"),
+        Movie(title: "Episode VI - Return of the Jedi"),
+        Movie(title: "Spider-Man: No Way Home")
+    ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Movies List")
+                .font(.largeTitle)
+            List(movies, id: \.id) { movie in
+                Text(movie.title)
+            }
+        }
     }
 }
 
